ae95fa3f58b6f2369357db3e0680ebc24271258f
Park JunHyeong <junbak1101a@naver.com>
2024-12-05 01:52:03 +0900

    5

--- train_yolo.py
+++ train_yolo.py
@@ -18,8 +18,13 @@ with open(coco_annotations_path, 'r') as f:
 # 이미지 파일 리스트
 image_files = [os.path.join(coco_images_path, img['file_name']) for img in coco_data['images']]
 
+train_data = []
+for image_file in tqdm(image_files):
+    img = cv2.imread(image_file)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    train_data.append(img_rgb)\
 
 # 모델 학습
-model.train(data='C:/Users/rainb/OneDrive/바탕 화면/final/data.yaml', epochs=50, batch=16)
+model.train(data='C:/Users/rainb/OneDrive/바탕 화면/final/data.yaml', epochs=50, batch=16, project=r'C:/Computer_Vision/results', name='yolo_training')
 
 print("모델 학습 완료")
